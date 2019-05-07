f756061358b014e73b3b6690ba982f3635953e5b
spyteamalex <spyteamalex@gmail.com>
2019-05-07 16:25:37 +0300

    Session(Mouse) Commit#34

--- src/Utils/MouseTracker.java
+++ src/Utils/MouseTracker.java
@@ -28,15 +28,18 @@ public class MouseTracker{
             s = new Stage();
             Pane p = new Pane();
             p.addEventFilter(MouseEvent.MOUSE_MOVED, this::mouseMoved);
-//            p.addEventFilter(MouseEvent.MOUSE_DRAGGED, this::mouseMoved);
+            p.addEventFilter(MouseEvent.MOUSE_DRAGGED, this::mouseMoved);
             p.addEventFilter(MouseEvent.MOUSE_PRESSED, this::mousePressed);
             p.addEventFilter(MouseEvent.MOUSE_RELEASED, this::mouseReleased);
-            p.addEventFilter(MouseEvent.MOUSE_CLICKED, this::mouseClicked);
             p.addEventFilter(ScrollEvent.SCROLL, this::mouseWheelMoved);
-//            p.addEventFilter(DragEvent.DRAG_DROPPED,this::dragDropped);
+            p.addEventFilter(DragEvent.DRAG_DROPPED,this::dragDropped);
             p.addEventFilter(KeyEvent.KEY_PRESSED, this::keyPressed);
             p.addEventFilter(KeyEvent.KEY_RELEASED, this::keyReleased);
-            p.addEventFilter(KeyEvent.KEY_TYPED, this::keyTyped);
+            p.focusedProperty().addListener((obs, wasFocused, isNowFocused) -> {
+                if (!isNowFocused) {
+                    p.requestFocus();
+                }
+            });
             s.setAlwaysOnTop(true);
             s.setMaximized(true);
             s.initStyle(StageStyle.UNDECORATED);
@@ -45,6 +48,7 @@ public class MouseTracker{
             });
             s.setScene(new Scene(p, 600, 600));
             s.show();
+            s.getScene().getRoot().requestFocus();
         });
     }
 
@@ -93,22 +97,18 @@ public class MouseTracker{
             for(Message m : messages){
                 sc.getSocket().send(new DatagramPacket(m.getArr(),m.getArr().length,sc.getAddress(),sc.getPort()));
             }
-            System.out.println(sc.getAddress().getHostAddress()+":"+sc.getPort());
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-    }
-
-    private void mouseClicked(MouseEvent t){
-        mousePressed(t);
-        mouseReleased(t);
     }
 
     public void mouseWheelMoved(ScrollEvent e){
         try{
             JSONObject msg = new JSONObject();
             msg.put("Type","mouseWheel");
-            msg.put("value",e.getDeltaY()/-10);
+            double value = -e.getDeltaY()/10;
+            value = value > 0 ? Math.ceil(value) : -Math.ceil(-value);
+            msg.put("value",value);
             Message[] messages = Message.getMessages(msg.toJSONString().getBytes(), MESSAGESIZE);
             for(Message m : messages){
                 sc.getSocket().send(new DatagramPacket(m.getArr(),m.getArr().length,sc.getAddress(),sc.getPort()));
@@ -128,8 +128,6 @@ public class MouseTracker{
                     sc.getSocket().send(new DatagramPacket(m.getArr(),m.getArr().length,sc.getAddress(),sc.getPort()));
                 }
                 s.close();
-                Thread.sleep(100);
-                sc.Stop();
 //            }else if(e.isAltDown() && e.getKeyCode() == KeyEvent.VK_S) {
 //                msg.put("Type", "swap");
 //                Message[] messages = Message.getMessages(msg.toJSONString().getBytes());
@@ -145,13 +143,13 @@ public class MouseTracker{
 //                f.dispose();
             }else{
                 msg.put("Type", "keyPressed");
-                msg.put("value", e.getCode());
+                msg.put("value", e.getCode().getCode());
                 Message[] messages = Message.getMessages(msg.toJSONString().getBytes(), MESSAGESIZE);
                 for(Message m : messages){
                     sc.getSocket().send(new DatagramPacket(m.getArr(),m.getArr().length,sc.getAddress(),sc.getPort()));
                 }
             }
-        } catch (InterruptedException | IOException e1) {
+        } catch (IOException e1) {
             e1.printStackTrace();
         }
     }
@@ -160,7 +158,7 @@ public class MouseTracker{
         try {
             JSONObject msg = new JSONObject();
             msg.put("Type", "keyReleased");
-            msg.put("value", e.getCode());
+            msg.put("value", e.getCode().getCode());
             Message[] messages = Message.getMessages(msg.toJSONString().getBytes(), MESSAGESIZE);
             for (Message m : messages) {
                 sc.getSocket().send(new DatagramPacket(m.getArr(), m.getArr().length, sc.getAddress(), sc.getPort()));
@@ -168,10 +166,5 @@ public class MouseTracker{
         } catch (IOException ex) {
             ex.printStackTrace();
         }
-    }
-
-    public void keyTyped(KeyEvent e) {
-        keyPressed(e);
-        keyReleased(e);
     }
 }
