ad323935e57edb9947bd09e2bbe5f9a080620a71
spyteamalex <spyteamalex@gmail.com>
2019-05-07 12:37:34 +0300

    Session(Mouse) Commit#30

--- src/Utils/MouseTracker.java
+++ src/Utils/MouseTracker.java
@@ -1,61 +1,58 @@
 package Utils;
 
+import javafx.application.Platform;
+import javafx.scene.Scene;
+import javafx.scene.input.*;
+import javafx.scene.layout.Pane;
+import javafx.stage.Stage;
+import javafx.stage.StageStyle;
 import org.json.simple.JSONObject;
 
-import javax.swing.*;
 import java.awt.*;
-import java.awt.event.*;
 import java.io.IOException;
 import java.net.DatagramPacket;
 
-public class MouseTracker extends Frame implements MouseListener, MouseMotionListener, MouseWheelListener, KeyListener {
+public class MouseTracker{
 
-    static int startX = 100,startY = 100;
     SessionClient sc;
     double width;
     double height;
-    JFrame f;
-    static Robot r;
-    static {
-        try {
-            r = new Robot();
-        } catch (AWTException e) {
-            e.printStackTrace();
-        }
-    }
+    Stage s;
     public MouseTracker(SessionClient sc) throws IOException, AWTException {
-        this.sc = sc;
-        Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
-        width = screenSize.getWidth();
-        height = screenSize.getHeight();
-        f = new JFrame("MouseListener");
-        f.setSize(600, 100);
-        f.setAlwaysOnTop(true);
-        f.setExtendedState(JFrame.MAXIMIZED_BOTH);
-        f.setUndecorated(true);
-        f.setVisible(true);
-        JPanel p = new JPanel();
-        p.setLayout(new FlowLayout());
-        JLabel icon = new JLabel(new ImageIcon("src/sample/Images/Icon.png"));
-        p.add(icon);
-        f.setBackground(Color.getColor("#202020"));
-        f.addMouseListener(this);
-        f.addMouseMotionListener(this);
-        f.addMouseWheelListener(this);
-        f.addKeyListener(this);
-        f.add(p);
-        f.setDefaultCloseOperation(JFrame.DO_NOTHING_ON_CLOSE);
-        f.setCursor(Toolkit.getDefaultToolkit().createCustomCursor(new ImageIcon(new byte[0]).getImage(),new Point(0,0), "custom"));
-        f.show();
+        Platform.runLater(()-> {
+            this.sc = sc;
+            Dimension screenSize = Toolkit.getDefaultToolkit().getScreenSize();
+            width = screenSize.getWidth();
+            height = screenSize.getHeight();
+            s = new Stage();
+            Pane p = new Pane();
+            p.addEventFilter(MouseEvent.MOUSE_MOVED, this::mouseMoved);
+//            p.addEventFilter(MouseEvent.MOUSE_DRAGGED, this::mouseMoved);
+            p.addEventFilter(MouseEvent.MOUSE_PRESSED, this::mousePressed);
+            p.addEventFilter(MouseEvent.MOUSE_RELEASED, this::mouseReleased);
+            p.addEventFilter(MouseEvent.MOUSE_CLICKED, this::mouseClicked);
+            p.addEventFilter(ScrollEvent.SCROLL, this::mouseWheelMoved);
+//            p.addEventFilter(DragEvent.DRAG_DROPPED,this::dragDropped);
+            p.addEventFilter(KeyEvent.KEY_PRESSED, this::keyPressed);
+            p.addEventFilter(KeyEvent.KEY_RELEASED, this::keyReleased);
+            p.addEventFilter(KeyEvent.KEY_TYPED, this::keyTyped);
+            s.setAlwaysOnTop(true);
+            s.setMaximized(true);
+            s.initStyle(StageStyle.UNDECORATED);
+            s.setResizable(false);
+            s.setOnCloseRequest((e) -> {
+            });
+            s.setScene(new Scene(p, 600, 600));
+            s.show();
+        });
     }
 
-    @Override
     public void mousePressed(MouseEvent e)
     {
         try {
             JSONObject msg = new JSONObject();
             msg.put("Type","mousePressed");
-            msg.put("Key",e.getButton());
+            msg.put("Key",e.getButton().ordinal());
             Message[] messages = Message.getMessages(msg.toJSONString().getBytes());
             for(Message m : messages){
                     sc.getSocket().send(new DatagramPacket(m.getArr(),m.getArr().length,sc.getAddress(),sc.getPort()));
@@ -65,13 +62,17 @@ public class MouseTracker extends Frame implements MouseListener, MouseMotionLis
         }
     }
 
-    @Override
+    public void dragDropped(DragEvent e){
+        MouseEvent me = new MouseEvent(MouseEvent.MOUSE_RELEASED,0,0,0,0, MouseButton.PRIMARY,0,true,true,true,true,true,true,true,true,true,true,null);
+        mouseReleased(me);
+    }
+
     public void mouseReleased(MouseEvent e)
     {
         try{
             JSONObject msg = new JSONObject();
             msg.put("Type","mouseReleased");
-            msg.put("Key",e.getButton());
+            msg.put("Key",e.getButton().ordinal());
             Message[] messages = Message.getMessages(msg.toJSONString().getBytes());
             for(Message m : messages){
                 sc.getSocket().send(new DatagramPacket(m.getArr(),m.getArr().length,sc.getAddress(),sc.getPort()));
@@ -81,57 +82,31 @@ public class MouseTracker extends Frame implements MouseListener, MouseMotionLis
         }
     }
 
-    @Override
-    public void mouseExited(MouseEvent e) {}
-
-    @Override
-    public void mouseEntered(MouseEvent e){}
-
-    @Override
-    public void mouseClicked(MouseEvent e){}
-
-    @Override
-    public void mouseMoved(MouseEvent e){
+    private void mouseMoved(MouseEvent t) {
         try {
-            if (e.getX() != startX || e.getY() != startY) {
-                JSONObject msg = new JSONObject();
-                msg.put("Type", "mouseMoved");
-                msg.put("X", e.getX() - startX);
-                msg.put("Y", e.getY() - startY);
-                Message[] messages = Message.getMessages(msg.toJSONString().getBytes());
-                for(Message m : messages){
-                    sc.getSocket().send(new DatagramPacket(m.getArr(),m.getArr().length,sc.getAddress(),sc.getPort()));
-                }
-                r.mouseMove(startX, startY);
-            }
-        } catch (IOException ex) {
-            ex.printStackTrace();
-        }
-    }
-
-    @Override
-    public void mouseDragged(MouseEvent e){
-        try{
             JSONObject msg = new JSONObject();
-            msg.put("Type","mouseMoved");
-            msg.put("X",e.getX()-startX);
-            msg.put("Y",e.getY()-startY);
+            msg.put("Type", "mouseMoved");
+            msg.put("X", t.getX());
+            msg.put("Y", t.getY());
             Message[] messages = Message.getMessages(msg.toJSONString().getBytes());
             for(Message m : messages){
                 sc.getSocket().send(new DatagramPacket(m.getArr(),m.getArr().length,sc.getAddress(),sc.getPort()));
             }
-            r.mouseMove(startX, startY);
         } catch (IOException ex) {
             ex.printStackTrace();
         }
     }
 
-    @Override
-    public void mouseWheelMoved(MouseWheelEvent e){
+    private void mouseClicked(MouseEvent t){
+        mousePressed(t);
+        mouseReleased(t);
+    }
+
+    public void mouseWheelMoved(ScrollEvent e){
         try{
             JSONObject msg = new JSONObject();
             msg.put("Type","mouseWheel");
-            msg.put("value",e.getWheelRotation());
+            msg.put("value",e.getDeltaY());
             Message[] messages = Message.getMessages(msg.toJSONString().getBytes());
             for(Message m : messages){
                 sc.getSocket().send(new DatagramPacket(m.getArr(),m.getArr().length,sc.getAddress(),sc.getPort()));
@@ -141,20 +116,16 @@ public class MouseTracker extends Frame implements MouseListener, MouseMotionLis
         }
     }
 
-    @Override
-    public void keyTyped(KeyEvent e) {}
-
-    @Override
     public void keyPressed(KeyEvent e) {
         try {
             JSONObject msg = new JSONObject();
-            if (e.isAltDown() && e.getKeyCode() == KeyEvent.VK_F4){
+            if (e.isAltDown() && e.getCode() == KeyCode.F4){
                 msg.put("Type", "finish");
                 Message[] messages = Message.getMessages(msg.toJSONString().getBytes());
                 for(Message m : messages){
                     sc.getSocket().send(new DatagramPacket(m.getArr(),m.getArr().length,sc.getAddress(),sc.getPort()));
                 }
-                f.dispose();
+                s.close();
                 Thread.sleep(100);
                 sc.Stop();
 //            }else if(e.isAltDown() && e.getKeyCode() == KeyEvent.VK_S) {
@@ -172,25 +143,22 @@ public class MouseTracker extends Frame implements MouseListener, MouseMotionLis
 //                f.dispose();
             }else{
                 msg.put("Type", "keyPressed");
-                msg.put("value", e.getKeyCode());
+                msg.put("value", e.getCode());
                 Message[] messages = Message.getMessages(msg.toJSONString().getBytes());
                 for(Message m : messages){
                     sc.getSocket().send(new DatagramPacket(m.getArr(),m.getArr().length,sc.getAddress(),sc.getPort()));
                 }
             }
-        } catch (InterruptedException e1) {
-            e1.printStackTrace();
-        } catch (IOException e1) {
+        } catch (InterruptedException | IOException e1) {
             e1.printStackTrace();
         }
     }
 
-    @Override
     public void keyReleased(KeyEvent e) {
         try {
             JSONObject msg = new JSONObject();
             msg.put("Type", "keyReleased");
-            msg.put("value", e.getKeyCode());
+            msg.put("value", e.getCode());
             Message[] messages = Message.getMessages(msg.toJSONString().getBytes());
             for (Message m : messages) {
                 sc.getSocket().send(new DatagramPacket(m.getArr(), m.getArr().length, sc.getAddress(), sc.getPort()));
@@ -198,5 +166,10 @@ public class MouseTracker extends Frame implements MouseListener, MouseMotionLis
         } catch (IOException ex) {
             ex.printStackTrace();
         }
+    }
+
+    public void keyTyped(KeyEvent e) {
+        keyPressed(e);
+        keyReleased(e);
     }
 }
